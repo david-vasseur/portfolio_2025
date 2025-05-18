ad81ccec15726ce4ad5ed7fa70f854ecb3523795
David <p5y4@laposte.net>
2025-05-18 10:00:31 +0200

    updated some css

--- src/app/__components/ux/Logo.tsx
+++ src/app/__components/ux/Logo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Logo({ className }: { className?: string }) {
     return (
         <div className={`${className} logo-filter rounded-[100%] border-2 border-[#5F606A] overflow-hidden`}>
-            <Image className="scale-135" src="/logo.jpeg" alt="logo dev" width={50} height={50} />
+            <Image className="scale-135 rounded-full" src="/logo.jpeg" alt="logo dev" width={50} height={50} />
         </div>
     )
 }
