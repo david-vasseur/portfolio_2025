750ac18453a644bcd978ff06152acdb71f00abc7
David <p5y4@laposte.net>
2025-05-19 09:47:46 +0200

    adding skill part

---
+++ src/app/__components/features/Skills.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { IconType } from 'react-icons';
+
+
+function Skills({ title, icons }: { title: string, icons: IconType[] }) {
+
+    return (
+        <div className=" w-[30vw] flex flex-col justify-center items-center gap-4 acc5">
+            <h3 className="p-2 xs:texy-xl sm:text-3xl text-[var(--text2)] font-black border-b-1 border-b-[var(--text-2)] border-l-1 border-l-[var(--text-2)]">{title}</h3>
+            <div className="flex gap-2">
+                {icons.map((Icon: IconType, index) => {
+                    return (
+                        <Icon key={index} className="text-[1rem] sm:text-[2rem] text-[#FFC9FF] neon-icons" />
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
--- src/app/__components/layout/SectionHome.tsx
+++ src/app/__components/layout/SectionHome.tsx
@@ -1,23 +1,46 @@
 "use client"
 
-import { useState } from "react";
-import Transitions from "../features/Transitions"
+import Skills from "../features/Skills";
+import { FaDocker, FaNode, FaReact, FaUbuntu } from "react-icons/fa6";
+import { SiExpress, SiNginx, SiTailwindcss } from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
+import { IconType } from "react-icons";
+import { TbBrandMongodb } from "react-icons/tb";
+
+type FetchedSkillsProps = {
+    id: number,
+    name: string,
+    content: IconType[]
+}
 
 export default function Page() {
 
-    const [isOpen, setIsOpen] = useState(false);
-    console.log(isOpen);
-    
-
-    const handleClick = () => {
-        setIsOpen((prev) => !prev);
-        setTimeout(() => {setIsOpen((prev) => !prev)}, 1000);
-    }
+    const fetchedSkills: FetchedSkillsProps[] = [
+        {
+            id: 0,
+            name: "Front-end",
+            content: [FaReact , SiTailwindcss , RiNextjsFill]
+        },
+        {
+            id: 1,
+            name: "Back-end",
+            content: [FaNode , SiExpress , TbBrandMongodb]
+        },
+        {
+            id: 2,
+            name: "DevOps",
+            content: [SiNginx , FaUbuntu , FaDocker]
+        }
+    ];
 
 	return (
-		<div className="flex flex-col gap-5">
-			<h2 onClick={() => handleClick()}>Section Home</h2>
-            <Transitions isOpen={isOpen} onClose={() => setIsOpen(false)} />
+		<div className="flex flex-col gap-5 items-center">
+			<h2>Section Home</h2>
+            <div className="flex justify-around">
+                {fetchedSkills.map(skill => (
+                    <Skills key={skill.id} title={skill.name} icons={skill.content} />
+                ))}
+            </div>         
 		</div>
 	)
 };
